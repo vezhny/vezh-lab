73a6a5af745485913772edf2290c3fef296946e3
Artyom.Vezhnovec <artsiom.vezhnavets@it-wk.ru>
2018-08-27 10:55:02 +0300

    #3: created run module. Server starts on port in application.properties file.

--- run/src/main/java/server/Server.java
+++ run/src/main/java/server/Server.java
@@ -1,12 +1,13 @@
-package vezhbank;
+package server;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Server {
 
     public static void main(String args[]) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Server.class, args);
     }
 }

b3f03435d81b9c69416cff09ea931664008a5f2b
Artyom.Vezhnovec <artsiom.vezhnavets@it-wk.ru>
2018-08-27 13:57:04 +0300

    #5: trying to inject hibernate into server through vezh_bank module

--- run/src/main/java/server/Server.java
+++ run/src/main/java/server/Server.java
@@ -1,9 +1,12 @@
 package server;
 
 
+import core.config.VezhBankConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@AutoConfigureAfter({VezhBankConfiguration.class})
 @SpringBootApplication
 public class Server {
 
---
+++ vezh_bank/src/main/java/core/config/VezhBankConfiguration.java
@@ -0,0 +1,29 @@
+package core.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan({"vezh_lab.jpa.vezh_bank"})
+public class VezhBankConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry resourceHandlerRegistry) {
+        resourceHandlerRegistry.addResourceHandler("/resources/**").addResourceLocations("WEB-INF/resources/");
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+//        internalResourceViewResolver.setRedirectHttp10Compatible(false);
+        return internalResourceViewResolver;
+    }
+
+}

a6fca2703e52b91a05f72b3912b87c957a099538
Artyom.Vezhnovec <artsiom.vezhnavets@it-wk.ru>
2018-09-25 16:52:35 +0300

    #7, #15, #18: get users tests

--- test_module/src/test/java/vezh_bank/controller/UserControllerTests.java
+++ test_module/src/test/java/vezh_bank/controller/UserControllerTests.java
@@ -1,5 +1,6 @@
 package vezh_bank.controller;
 
+import core.dto.UserDTO;
 import core.dto.UserRoleDTO;
 import core.json.UserAddress;
 import core.json.UserConfig;
@@ -18,6 +19,7 @@ import vezh_bank.constants.ExceptionMessages;
 import vezh_bank.constants.RequestParams;
 import vezh_bank.constants.Urls;
 import vezh_bank.constants.UserDefault;
+import vezh_bank.controller.providers.users.get.GetUsersArgumentsProvider;
 import vezh_bank.controller.providers.users.registration.RegisterUserFailArgumentsProvider;
 import vezh_bank.controller.providers.users.registration.RegisterUserSuccessAgrumentsProvider;
 import vezh_bank.controller.providers.users.registration.RegistrationClientArgumentsProvider;
@@ -396,5 +398,119 @@ public class UserControllerTests extends ControllerTest {
         userAsserts.checkNumberOfUsers(7, users.getUsers().size());
         httpAsserts.checkCurrentPage(1, response);
         httpAsserts.checkPagesCount(1, response);
+    }
+
+    @Link(url = "https://github.com/vezhny/vezh-lab/issues/18")
+    @Description("Get users when user id absent")
+    @Test
+    public void getUsersAbsentUserId() {
+        testUtils.logTestStart("Get users when user id absent");
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+
+        MockHttpServletResponse response = httpGet(Urls.USERS, params);
+
+        httpAsserts.checkResponseCode(400, response.getStatus());
+        httpAsserts.checkExceptionMessage(ExceptionMessages.USER_ID_MUST_PRESENT, response);
+    }
+
+    @Link(url = "https://github.com/vezhny/vezh-lab/issues/18")
+    @Description("Get users when user id can't be a number")
+    @Test
+    public void getUsersUserIdCanNotBeNumber() {
+        testUtils.logTestStart("Get users when user id can't be a number");
+
+        String userId = "x";
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.set(RequestParams.USER_ID, userId);
+
+        MockHttpServletResponse response = httpGet(Urls.USERS, params);
+
+        httpAsserts.checkResponseCode(400, response.getStatus());
+        httpAsserts.checkExceptionMessage(String.format(ExceptionMessages.VALUE_CAN_NOT_BE_A_NUMBER, userId), response);
+    }
+
+    @Link(url = "https://github.com/vezhny/vezh-lab/issues/18")
+    @Description("Get users when user doesn't exist")
+    @Test
+    public void getUsersUserDoesNotExist() {
+        testUtils.logTestStart("Get users when user doesn't exist");
+
+        String userId = "999";
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.set(RequestParams.USER_ID, userId);
+
+        MockHttpServletResponse response = httpGet(Urls.USERS, params);
+
+        httpAsserts.checkResponseCode(400, response.getStatus());
+        httpAsserts.checkExceptionMessage(String.format(ExceptionMessages.USER_DOES_NOT_EXIST, userId), response);
+    }
+
+    @Link(url = "https://github.com/vezhny/vezh-lab/issues/18")
+    @Description("Get users when user is client")
+    @Test
+    public void getUsersUserIsClient() {
+        testUtils.logTestStart("Get users when user is client");
+
+        String userId = String.valueOf(testUtils.createClient(serviceProvider.getDataBaseService()));
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.set(RequestParams.USER_ID, userId);
+
+        MockHttpServletResponse response = httpGet(Urls.USERS, params);
+
+        httpAsserts.checkResponseCode(400, response.getStatus());
+        httpAsserts.checkExceptionMessage(ExceptionMessages.THIS_OPERATION_IS_NOT_AVAILABLE_FOR_CLIENTS, response);
+    }
+
+    @Link(url = "https://github.com/vezhny/vezh-lab/issues/18")
+    @Description("Get users parametrized")
+    @ArgumentsSource(GetUsersArgumentsProvider.class)
+    @ParameterizedTest
+    public void getUsers(int requiredPage, String login, String role, String blocked, String data,
+                         int expectedUsersCount, int expectedCurrentPage, int expectedNumberOfPages) throws UnsupportedEncodingException {
+        testUtils.logTestStart("Get users parametrized test");
+        int userId = testUtils.createNotAClient(serviceProvider.getDataBaseService());
+
+        List<UserRole> userRoles = serviceProvider.getDataBaseService().getRoleDao().selectAll();
+
+        UserAddress address1 = new UserAddress("country1", "region1", "city1", "street1", "23", "111");
+        UserAddress address2 = new UserAddress("country2", "region2", "city2", "street2", "24", "113");
+        UserAddress address3 = new UserAddress("country3", "region3", "city3", "street3", "25", "115");
+
+        UserData userData1 = new UserData("firstName1", "middleName1", "patronymic1", "10.10.1999",
+                address1, "+36985215", "test1@test.com");
+        UserData userData2 = new UserData("firstName2", "middleName2", "patronymic2", "10.08.1999",
+                address2, "+36985217", "test2@test.com");
+        UserData userData3 = new UserData("firstName3", "middleName3", "patronymic3", "10.04.1999",
+                address3, "+36985219", "test3@test.com");
+
+        serviceProvider.getUserService().addUser(new UserDTO("login1", "password", userRoles.get(0), userData1));
+        serviceProvider.getUserService().addUser(new UserDTO("login2", "password", userRoles.get(1), userData2));
+        serviceProvider.getUserService().addUser(new UserDTO("login3", "password", userRoles.get(2), userData3));
+        serviceProvider.getUserService().addUser(new UserDTO("login4", "password", userRoles.get(0), userData1));
+        serviceProvider.getUserService().addUser(new UserDTO("login5", "password", userRoles.get(1), userData2));
+        serviceProvider.getUserService().addUser(new UserDTO("login6", "password", userRoles.get(2), userData3));
+        serviceProvider.getUserService().addUser(new UserDTO("login7", "password", userRoles.get(0), userData1));
+        serviceProvider.getUserService().addUser(new UserDTO("login8", "password", userRoles.get(1), userData2));
+        serviceProvider.getUserService().addUser(new UserDTO("login9", "password", userRoles.get(2), userData3));
+        serviceProvider.getUserService().addUser(new UserDTO("login10", "password", userRoles.get(0), userData1));
+        serviceProvider.getUserService().addUser(new UserDTO("login11", "password", userRoles.get(1), userData2));
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.set(RequestParams.USER_ID, String.valueOf(userId));
+        params.set(RequestParams.REQUIRED_PAGE, String.valueOf(requiredPage));
+        params.set(RequestParams.LOGIN, login);
+        params.set(RequestParams.ROLE, role);
+        params.set(RequestParams.BLOCKED, blocked);
+        params.set(RequestParams.USER_DATA, data);
+
+        MockHttpServletResponse response = httpGet(Urls.USERS, params);
+
+        httpAsserts.checkResponseCode(200, response.getStatus());
+
+        Users users = gson.fromJson(response.getContentAsString(), Users.class);
+        userAsserts.checkNumberOfUsers(expectedUsersCount, users.getUsers().size());
+        httpAsserts.checkCurrentPage(expectedCurrentPage, response);
+        httpAsserts.checkPagesCount(expectedNumberOfPages, response);
     }
 }
---
+++ test_module/src/test/java/vezh_bank/controller/providers/users/get/GetUsersArgumentsProvider.java
@@ -0,0 +1,47 @@
+package vezh_bank.controller.providers.users.get;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import vezh_bank.enums.Role;
+
+import java.util.stream.Stream;
+
+public class GetUsersArgumentsProvider implements ArgumentsProvider {
+
+    /**
+     * params: requiredPage, login, role, blocked, data
+     * expectedUsersCount, expectedCurrentPage, expectedNumberOfPages
+     * @param extensionContext
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) throws Exception {
+        return Stream.of(
+                Arguments.of(1, "", "", "", "", 12, 1, 1),
+                Arguments.of(2, "", "", "", "", 12, 1, 1),
+                Arguments.of(0, "", "", "", "", 12, 1, 1),
+                Arguments.of(-1, "", "", "", "", 12, 1, 1),
+                Arguments.of(1, "login", "", "", "", 11, 1, 1),
+                Arguments.of(1, "login1", "", "", "", 3, 1, 1),
+                Arguments.of(1, null, "", "", "", 12, 1, 1),
+                Arguments.of(1, "", Role.CLIENT.toString(), "", "", 4, 1, 1),
+                Arguments.of(1, "", Role.ADMIN.toString(), "", "", 3, 1, 1),
+                Arguments.of(1, "", Role.EMPLOYEE.toString(), "", "", 5, 1, 1),
+                Arguments.of(1, "", null, "", "", 12, 1, 1),
+                Arguments.of(1, "", "", "", "firstName", 12, 1, 1),
+                Arguments.of(1, "", "", "", "firstName1", 4, 1, 1),
+                Arguments.of(1, "", "", "", "1999", 11, 1, 1),
+                Arguments.of(1, "", "", "", "middleName", 12, 1, 1),
+                Arguments.of(1, "", "", "", "middleName1", 4, 1, 1),
+                Arguments.of(1, "", "", "", "patronymic", 12, 1, 1),
+                Arguments.of(1, "", "", "", "patronymic1", 4, 1, 1),
+                Arguments.of(1, "", "", "", "10.10.1999", 4, 1, 1),
+                Arguments.of(1, "", "", "", "country", 12, 1, 1),
+                Arguments.of(1, "", "", "", "country1", 4, 1, 1),
+                Arguments.of(1, "", "", "", "+36985219", 3, 1, 1),
+                Arguments.of(1, "", "", "", "test3@test.com", 3, 1, 1)
+        );
+    }
+}

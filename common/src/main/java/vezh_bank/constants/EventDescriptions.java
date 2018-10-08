b27c7995304bf63600d6812c51b0e044f79f3733
Artyom.Vezhnovec <artsiom.vezhnavets@it-wk.ru>
2018-10-08 14:19:04 +0300

    #7, #15, #36: bug fixed

--- common/src/main/java/vezh_bank/constants/EventDescriptions.java
+++ common/src/main/java/vezh_bank/constants/EventDescriptions.java
@@ -6,6 +6,7 @@ public class EventDescriptions {
     public static final String REGISTERED_USER_WITH_LOGIN = "Registered user with login: %s";
     public static final String USER_DELETED_USER_WITH_LOGIN = "User \"%s\" (%s) deleted another user \"%s\" (%s)";
     public static final String USER_HAS_BEEN_UPDATED_BY = "User \"%s\" (%s) has been updated by \"%s\" (%s)";
+    public static final String USER_SIGNED_IN = "User \"%s\" signed in";
 
     public static String registeredUserWithLogin(String login) {
         return format(REGISTERED_USER_WITH_LOGIN, login);
@@ -18,5 +19,9 @@ public class EventDescriptions {
     public static String userHasBeenUpdatedBy(String updaterLogin, String updaterRole,
                                               String victimLogin, String victimRole) {
         return format(USER_HAS_BEEN_UPDATED_BY, updaterLogin, updaterRole, victimLogin, victimRole);
+    }
+
+    public static String userSignedIn(String login) {
+        return format(USER_SIGNED_IN, login);
     }
 }
